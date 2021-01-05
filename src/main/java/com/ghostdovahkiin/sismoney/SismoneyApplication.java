34ee04f773c946848345e93daa73674903cd2476
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2021-01-05 10:47:36 -0300

    :key: chore: Generated project

---
+++ src/main/java/com/ghostdovahkiin/sismoney/SismoneyApplication.java
@@ -0,0 +1,13 @@
+package com.ghostdovahkiin.sismoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SismoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SismoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghostdovahkiin/sismoney/SismoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghostdovahkiin.sismoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SismoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

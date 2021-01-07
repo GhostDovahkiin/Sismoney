a340a00235869a785fe86019a76107a22614ebc3
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2021-01-07 12:53:25 -0300

    :key: chore: New category added

--- src/main/resources/db/migration/V01__create_and_save_categories.sql
+++ src/main/resources/db/migration/V01__create_and_save_categories.sql
@@ -1,2 +1,3 @@
 CREATE TABLE category( id IDENTITY NOT NULL PRIMARY KEY, category_name VARCHAR NOT NULL);
 INSERT INTO category(category_name) VALUES('Netflix');
+INSERT INTO category(category_name) VALUES('Amazon');

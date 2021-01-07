0f278cc7af7538f02693ee81bdf0733b74db1490
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2021-01-07 16:42:57 -0300

    :recycle: refactor: Remove unused import

--- src/main/java/com/ghostdovahkiin/sismoney/model/Category.java
+++ src/main/java/com/ghostdovahkiin/sismoney/model/Category.java
@@ -6,8 +6,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-
 @Entity
 @Table(name = "category")
 public class Category {

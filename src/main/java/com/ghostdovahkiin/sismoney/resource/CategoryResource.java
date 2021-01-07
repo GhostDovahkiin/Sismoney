b3dd6851307407dce796aa748b472f49bf433ce8
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2021-01-07 12:53:06 -0300

    :fire: feat: Return ResponseEntity for empty collection and POST Working

--- src/main/java/com/ghostdovahkiin/sismoney/resource/CategoryResource.java
+++ src/main/java/com/ghostdovahkiin/sismoney/resource/CategoryResource.java
@@ -6,8 +6,13 @@ import com.ghostdovahkiin.sismoney.model.Category;
 import com.ghostdovahkiin.sismoney.repository.CategoryRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +22,14 @@ public class CategoryResource {
   private CategoryRepository categoryRepository;
 
   @GetMapping
-  public List<Category> list() {
-    return categoryRepository.findAll();
+  public ResponseEntity<?> list() {
+    List<Category> categories = categoryRepository.findAll();
+    return !categories.isEmpty() ? ResponseEntity.ok(categories) : ResponseEntity.noContent().build();
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public void create(@RequestBody Category category) {
+    categoryRepository.save(category);
   }
 }

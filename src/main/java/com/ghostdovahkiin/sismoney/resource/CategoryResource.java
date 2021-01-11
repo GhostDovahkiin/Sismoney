777b0f7a14ebbb5b10caba384692785a7eb91c3c
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2021-01-11 10:53:43 -0300

    :fire: feat: Return not found when category not exists

--- src/main/java/com/ghostdovahkiin/sismoney/resource/CategoryResource.java
+++ src/main/java/com/ghostdovahkiin/sismoney/resource/CategoryResource.java
@@ -2,6 +2,7 @@ package com.ghostdovahkiin.sismoney.resource;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,13 +10,12 @@ import com.ghostdovahkiin.sismoney.model.Category;
 import com.ghostdovahkiin.sismoney.repository.CategoryRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -38,5 +38,11 @@ public class CategoryResource {
         .toUri();
     response.setHeader("Location", uri.toASCIIString());
     return ResponseEntity.created(uri).body(savedCategory);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<?> searchById(@PathVariable Long id) {
+    Optional<Category> foundCategory = categoryRepository.findById(id);
+    return !foundCategory.isEmpty() ? ResponseEntity.ok(foundCategory) : ResponseEntity.notFound().build();
   }
 }
